db19d002acbbb7b0d2acd32310ae27edcd085814
linus <linus@DESKTOP-8GO3C6M>
2021-11-04 15:35:04 +0100

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+
+
+#include <iostream>
+using namespace std;
+int numba;
+
+int main()
+{
+    cout << "Hello motherfucca time fo you to enter a numba\n";
+    cin >> numba;
+    cout << numba + " is this yo numba ? ";
+}
+
