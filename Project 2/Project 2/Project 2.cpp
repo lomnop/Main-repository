02e4a7243fea505ca7d9fcad053e786a32f6208e
Bipedi Bopedi <linusgrahammar@hotmail.se>
2021-11-05 10:47:46 +0100

    Continued improvement

--- Project 2/Project 2/Project 2.cpp
+++ Project 2/Project 2/Project 2.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 using namespace std;
-
+/*
 int main()
 {
     cout << "Input name\n";
@@ -29,4 +29,34 @@ int main()
 
     
 }
+*/
 
+
+int main()
+{
+    char name[20];
+    int age;
+    char gender;
+    char adress[20];
+    int married;
+    int height;
+
+    
+    printf_s("Input name\n");
+    scanf_s("%s", name,_countof(name));
+    printf_s("Input Age\n");
+    scanf_s(" %d", &age);
+    printf_s("Input Gender (M/F)\n");
+    scanf_s(" %c", &gender);
+    printf_s("Input Adress\n");
+    scanf_s(" %s", adress,_countof(adress));
+    printf_s("married? (True=1/False=0)\n");
+    scanf_s(" %d", &married);
+    printf_s("Input Height\n");
+    scanf_s(" %d", &height);
+
+
+
+
+    printf_s("\nName = %s\nAge = %d\nGender = %c\nAdress =%s\nMarried = %d\nHeight = %d\n", name,age,gender,adress,married,height);
+}
