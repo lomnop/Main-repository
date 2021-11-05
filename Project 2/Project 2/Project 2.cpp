5a13b5e8b01e48a9b9ba48cedcd963f401471795
Bipedi Bopedi <linusgrahammar@hotmail.se>
2021-11-05 07:33:24 +0100

    wawawa

--- Project 2/Project 2/Project 2.cpp
+++ Project 2/Project 2/Project 2.cpp
@@ -5,6 +5,28 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    cout << "Input name\n";
+    string name;
+    cin >> name;
+    cout << "Input age\n";
+    int age;
+    cin >> age;
+    cout << "Input gender m/f\n";
+    char gender;
+    cin >> gender;
+    cout << "Input adress\n";
+    string adress;
+    cin >> adress;
+    cout << "Married? (True=1/False=0)\n";
+    bool married;
+    cin >> married;
+    cout << "Input height\n";
+    int height;
+    cin >> height;
+
+
+    cout << endl <<"Name: "<<name<<endl<<"Age: "<<age<<endl<<"Gender: "<<gender<<endl<<"Adress: "<<adress<<endl<<"Married?: "<<married<<endl<<"Height: "<<height<<"cm"<<endl;
+
+    
 }
 
