461461dddd5eceb9c4995511a2f787c66f6128b6
Bipedi Bopedi <linusgrahammar@hotmail.se>
2021-11-05 07:57:03 +0100

    Test fail pogogo

---
+++ Task 5/Task 5/Task 5.cpp
@@ -0,0 +1,27 @@
+
+
+#include <iostream>
+
+int main()
+{
+    printf_s("Input name\n");
+    char name[20];
+    scanf_s(name);
+    printf_s("Input age\n");
+    int age[5];
+    scanf_s(age);
+    printf_s("Input gender\n");
+    char gender;
+    scanf_s(gender);
+    printf_s("Input adress\n");
+    string adress;
+    scanf_s(adress);
+    printf_s("Input marrried?(True=1/False=0)\n");
+    int married;
+    scanf_s(name);
+    printf_s("Input Height\n");
+    char name[20];
+    scanf_s(name);
+}
+
+
